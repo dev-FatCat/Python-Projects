90f229915efafe979a691ab006f9633c408558f4
dev-FatCat <125044420+dev-FatCat@users.noreply.github.com>
2023-02-10 17:29:37 -0500

    Update Space-Invaders.py

--- Space-Invaders.py
+++ Space-Invaders.py
@@ -14,8 +14,23 @@ green = (0, 255, 0)
 
 red = (255, 0, 0)
 
+class Ship(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface([50, 25])
+        self.image.fill(green)
+        self.rect = self.image.get_rect()
+        self.live = 5
+    def draw(self):
+        win.blit(self.image, (self.rect.x, self.rect.y))
+
+ship = Ship()
+ship.rect.x = 375
+ship.rect.y = 650
+
 def redraw():
     win.fill(black)
+    ship.draw()
     pygame.display.update()
 
 run = True
@@ -27,6 +42,13 @@ while run:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
+
+    key = pygame.key.get_pressed()
+    if key[pygame.K_LEFT]:
+        ship.rect.x += -10
+    if key[pygame.K_RIGHT]:
+        ship.rect.x += 10
+
     redraw()
 
 pygame.quit
